ed85baac2765f84a71a1a57d8abf25867742b135
eoinlynchcodes <eoin95lynch@gmail.com>
2020-08-24 15:22:27 +0100

    Close

---
+++ openPanel.js
@@ -0,0 +1,12 @@
+function openPanelRose() {
+    console.log('yes');
+    var element = document.getElementById("rose");
+    element.classList.toggle("openPanel");
+  }
+
+
+  function openPanelMargaux() {
+    console.log('yes');
+    var element = document.getElementById("margaux");
+    element.classList.toggle("openPanel");
+  }
